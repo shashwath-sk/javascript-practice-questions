9ce8e840e7e0536ba0dfe97325dc579cd7013b6a
shashwath sk <shashukarkera01@gmail.com>
2023-01-15 20:02:53 +0530

    feat: add feature to find longest string from an array

--- stringUtilities.js
+++ stringUtilities.js
@@ -42,4 +42,32 @@ const string = 'MeRRy hAD a LITTle lAMp'
 // console.log(changeCaseArrow(changeStringCase))
 
 
+// Q4. Write a JavaScript function (both regular and arrow) which takes an array of string as parameter ,
+// to find the longest string from a given array (read about string methods)
+
+
+
+function longestString(arrayOfString)
+{
+   let longest=''
+   arrayOfString.forEach(string=>
+    {
+       string.length>longest.length?longest=string:null
+    })
+    return longest
+}
+
+const longestStringArrow=arrayOfString=>
+{
+    let longest=''
+   arrayOfString.forEach(string=>
+    {
+       string.length>longest.length?longest=string:null
+    })
+    return longest
+}
+
+const arrayOfString = ["we", "love", "code", "academy"]
+console.log(longestString(arrayOfString))
+console.log(longestStringArrow(arrayOfString))
 
